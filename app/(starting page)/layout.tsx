78958b5f096c90d4cfc04b91120400fc89db38da
Gun Slinger <sengsengly3@gmail.com>
2025-11-24 13:27:25 +0700

    start

---
+++ app/(starting page)/layout.tsx
@@ -0,0 +1,24 @@
+import Sidebar from "@/components/sidebar";
+import Navbar from "@/components/navbar";
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="min-h-screen min-w-screen bg-white text-black">
+      <div className="fixed top-0 left-0 right-0 h-16 bg-white border-b z-20">
+        <Navbar />
+      </div>
+      <div className="flex pt-16 h-screen">
+        <div className="w-1/6 h-full bg-white border-r">
+          <Sidebar />
+        </div>
+        <div className="w-5/6 overflow-auto">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+    <div className="h-full w-full">
+      Sidebar
+    </div>
+  )
+}
